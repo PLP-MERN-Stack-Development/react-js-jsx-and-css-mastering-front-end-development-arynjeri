016f0160c258cdfe1fe6b829acf9d2e1fd46a0af
arynjeri <njeriary@gmail.com>
2025-10-21 10:58:37 +0300

    set base for Github pages

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,8 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  base: '/react-js-jsx-and-css-mastering-front-end-development-arynjeri/',
+  plugins: [react(), tailwindcss()],
 })
