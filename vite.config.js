cfc3711d0e954a57cb092285ea69b686ab595835
arynjeri <njeriary@gmail.com>
2025-10-21 12:12:01 +0300

    fixing dependancies

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
-  base: '/react-js-jsx-and-css-mastering-front-end-development-arynjeri/',
+  base: '/',
   plugins: [react(), tailwindcss()],
 })
